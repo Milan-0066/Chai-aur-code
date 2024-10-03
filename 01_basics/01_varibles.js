2e352c875f9c2d0854dfa97f7678537d503f3602
Manoranjan rout <158200960+Milan-0066@users.noreply.github.com>
2024-10-03 21:26:00 +0530

    first commit

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail = "rout33@gmail.com";
+var accountPassword = "123456";
+accountCity = "jajpur";
+
+//accountId = 2
+accountEmail = "ca@gmail.com";
+accountPassword = "5555666";
+accountCity = "puri";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+Preffer not to use var in work files
+because of issue in block scope and functinoal scope.
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // terat all JS code newer version.
+
+// alert (3 + 3) we are using nodejs, not browser.
+let name = "milan";
+let age = 20 ;
+let isLoggedIn = false;
+let state = null;
+
+
+// number = 12346...
+// bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+// undefined =
+//symbol = unique
+
+
+// object = 
+
+
+console.log(typeof "milan");
+
