da243c8d6ff4f84fd2050bb8d64dfe498921b0d6
Manoranjan rout <158200960+Milan-0066@users.noreply.github.com>
2024-10-03 22:14:07 +0530

    new file added

---
+++ 01_basics/03_convOpt.js
@@ -0,0 +1,29 @@
+/*let score = undefined ;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+//"33" => 33;
+//"33abc" => NaN;
+// true => 1; false => 0;
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// "0" => true boolean value;
+//"1" -> is true boolean value;
+//0 => is false bollean value;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
